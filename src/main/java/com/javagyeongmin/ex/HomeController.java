7b049fd09d41697c2c22e05896f543a7864e3efc
경민 <rodrng@gmail.com>
2022-04-14 16:19:19 +0900

    -

---
+++ src/main/java/com/javagyeongmin/ex/HomeController.java
@@ -0,0 +1,39 @@
+package com.javagyeongmin.ex;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String testImage(Locale locale, Model model) { // 메소드 이름이랑 뷰 이름 같게 한다 
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "testImage"; // view의 이름 // ㄱㅣ존 home에서 testImage 테스트하려고 만든걸로 변경함
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/testImage.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>이미지 불러오기 연습</title>
+</head>
+<body>
+	<h2>재미있는 spring framework!!</h2>
+	<br>
+	<img src="/ex/resources/img/dog.png">
+</body>
+</html>
